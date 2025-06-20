34d0e6b5ae040f6cb03e9ec54c6345d2822ec5b1
ARDI FIRMANSYAH <ardi.firmansyah@sfi.itochu.co.id>
2025-06-20 10:05:38 +0700

    test 5

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintln(w, "Halo dari Go CI/CD dari ardi !!!")
+		 fmt.Fprintln(w, "Setelah ini nampil maka ci cd nya berhasil")
     })
 
     fmt.Println("Server running on :8080")
